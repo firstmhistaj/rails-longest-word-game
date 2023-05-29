8b7d7b443510ff394b662cf235291bc8d05def63
folorunso oluwatobi <firstmhistaj@gmail.com>
2023-05-29 15:40:18 +0100

    games

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def new
+    @letters = ('A'..'Z').to_a.sample(10)
+  end
+
+  def score
+    word = params[:word]
+    letters = params[:letters]
+
+    # Check if the word can be built out of the original grid
+    if word.chars.all? { |char| word.count(char) <= letters.count(char) }
+      # Check if the word is a valid English word
+      response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+      word_valid = JSON.parse(response.read)
+
+      if word_valid
+        @result = "Congratulations! #{word.upcase} is a valid English word!"
+      else
+        @result = "Sorry, #{word.upcase} does not seem to be a valid English word."
+      end
+    else
+      @result = "Sorry, #{word.upcase} can't be built out of the original grid."
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/new', to: 'games#new', as: 'new_game'
+  post '/score', to: 'games#score', as: 'score_game'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
